83ed0560268d9f4025dc97f92572661acf9af95c
cultstatue <saltedchurros@gmail.com>
2022-08-24 16:27:14 -0400

    added basic dashboard forms, logic yet to capture input

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Game, Achievements} = require('../models');
+const { findAll } = require('../models/User');
 
+// load up the dashboard
 router.get('/', (req, res) => {
-    res.render('dashboard')
+    
+    // send current games database
+    Game.findAll({
+        attributes: ["id", "game_title"],
+    })
+    .then(dbGamesData => {
+        const games = dbGamesData.map(game => game.get({ plain:true }));
+        res.render('dashboard', { games });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
+
 
 module.exports = router;
