05404aad88c2f8f426a5cb3a204f450d861e79d5
Claire Cashmore <clairecashmore2017@gmail.com>
2022-08-24 15:20:13 -0400

    successful login route with save session variables

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,18 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
-router.get('/', (req, res) =>{
-
-    res.render('homepage')
-
-})
-
-router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //   res.redirect('/');
-    //   return;
-    // }
-    res.render('login');
+router.get("/", (req, res) => {
+  console.log(req.session);
+  res.render("homepage");
 });
-  
+
+router.get("/login", (req, res) => {
+  // if (req.session.loggedIn) {
+  //   res.redirect('/');
+  //   return;
+  // }
+  res.render("login");
+});
 
 module.exports = router;
