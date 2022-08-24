74b7e75f1600ee4c3405e18838ddb466e49e072e
cultstatue <saltedchurros@gmail.com>
2022-08-24 17:58:57 -0400

    added some logic for checking if a game is already in the database

---
+++ public/javascript/createpost.js
@@ -0,0 +1,100 @@
+// logic to create a new achievement
+async function createAchievement(achievement, game) {
+   
+    const genre = "test"
+
+    const response = await fetch('/api/achievements', {
+
+        method: 'POST',
+        body: JSON.stringify({
+
+            achievement,
+            game,
+            genre
+
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+          }
+    })
+
+    if(response.ok) {
+
+        console.log("new achievement recorded!")
+    } else {
+        alert(response.statusText)
+    }
+}
+
+
+// logic to check games database and return games as an array
+async function checkGames(game) {
+
+    const response = await fetch('/api/games', {
+        method: 'GET',
+        headers: {
+
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    if(response.ok) {
+
+        response.json()
+        .then(function(data) {
+            const gameArray = data.map(({ game_title }) => game_title);
+
+            if(gameArray.includes(game)) {
+
+                console.log("no game added")
+            } else {
+
+                createGame(game)
+
+            }
+
+            
+        })
+
+    } else {
+        alert(response.statusText)
+    }
+
+}
+
+// logic to create a new game
+async function createGame(game) {
+
+    const response = await fetch('/api/games', {
+
+        method: 'POST',
+        body: JSON.stringify({
+            game
+        }),
+        headers: {
+
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        console.log("game added!")
+    } else {
+        alert(response.statusText)
+    }
+}
+
+
+
+async function newPostHandler(event) {
+    event.preventDefault();
+
+    // grab form values
+    const achievement = document.querySelector('input[id="post-title-input"]').value
+    const game = document.querySelector('input[id="game-select-input"]').value
+
+    checkGames(game);
+    
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newPostHandler)
