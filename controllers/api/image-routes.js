c929df63f6810a5bcfeed99bbe90589ae4853eb9
Claire Cashmore <clairecashmore2017@gmail.com>
2022-08-26 22:05:35 -0400

    very unfinished, do not use

--- controllers/api/image-routes.js
+++ controllers/api/image-routes.js
@@ -1,27 +1,32 @@
 const router = require("express").Router();
-const { User, Post, Like, Comment, Achievements, Image } = require("../../models");
+const {
+  User,
+  Post,
+  Like,
+  Comment,
+  Achievements,
+  Image,
+} = require("../../models");
 
-router.get('/', (req, res) => {
-
-    Image.findAll({
-
-    })
-    .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-router.post('/', (req, res) => {
-
-    Image.create({
-        img_url: req.body.img_url
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
+router.get("/", (req, res) => {
+  Image.findAll({})
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
-})
+});
+router.post("/", (req, res) => {
+  //using uploader when image is trying to be created
+
+  Image.create({
+    img_url: req.body.img_url,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
---
+++ public/javascript/upload.js
@@ -0,0 +1,13 @@
+const uploader = new Uploader({
+  // Get production API keys from Upload.io
+  apiKey: "public_FW25au47S9ruZD1ciduXmCQcMLHc",
+});
+async function onFileSelected(event) {
+  const [file] = event.target.files;
+  const { fileUrl } = await upload.uploadFile({
+    file,
+    onBegin: ({ cancel }) => console.log("File upload started!"),
+    onProgress: ({ progress }) => console.log(`File uploading... ${progress}%`),
+  });
+  console.log(`File uploaded! ${fileUrl}`);
+}
