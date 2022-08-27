9fec0425042cf855e25bbd6a7656f0c42bcbf285
cultstatue <saltedchurros@gmail.com>
2022-08-27 00:31:09 -0400

    changed the dashboard route to serve posts and games

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,21 +1,127 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment, Game, Achievements } = require("../models");
+const { Post, User, Comment, Game, Achievements, Image } = require("../models");
 
 // load up the dashboard
 router.get("/", (req, res) => {
-  // send current games database
-  Game.findAll({
+
+  const gameData = Game.findAll({
     attributes: ["id", "game_title"],
   })
-    .then((dbGamesData) => {
-      const games = dbGamesData.map((game) => game.get({ plain: true }));
-      res.render("dashboard", { games });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+
+  const postData = Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'achievement_id',
+      'created_at'  
+      ],
+    include: [
+      {
+        model: Image,
+        attributes: ["id", "img_url"],
+      },
+      {
+        model: Achievements,
+        attributes: ["id", "title"],
+      },
+      {
+        model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      }
+    ]
+  })
+
+  Promise
+  .all([gameData, postData])
+  .then(response => {
+    const games = response[0].map((game) => game.get({ plain: true }));
+
+    const posts = response[1].map((post) => post.get({ plain: true }));
+    
+    res.render('dashboard', { games, posts })
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  // Promise.all(
+
+  //   Game.findAll({
+  //     attributes: ["id", "game_title"],
+  //   }),
+  //   Post.findAll({
+  //     where: {
+  //       // use the ID from the session
+  //       user_id: req.session.user_id
+  //     },
+  //     attributes: [
+  //       'id',
+  //       'title',
+  //       'achievement_id',
+  //       'image_id',
+  //       'created_at'  
+  //     ],
+  //     include: [
+  //       {
+  //         model: Image,
+  //         attributes: ["id", "img_url"],
+  //       },
+  //       {
+  //         model: Achievements,
+  //         attributes: ["id", "title"],
+  //       },
+  //       {
+  //         model: Comment,
+  //         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+  //         include: {
+  //           model: User,
+  //           attributes: ["username"],
+  //         },
+  //       },
+  //       {
+  //         model: User,
+  //         attributes: ["username"],
+  //       }
+  //     ]
+  //   })
+
+  // )
+  // .then((dbData) => {
+
+  //   const games = dbData.map((game) => game.get({ plain: true }));
+  //   const posts = dbData.map((post) => post.get({ plain: true }));
+
+  //   res.render('dashboard', { posts, games })
+  // })
+
+
+
+
+  // // send current games database
+  // Game.findAll({
+  //   attributes: ["id", "game_title"],
+  // })
+  //   .then((dbGamesData) => {
+  //     const games = dbGamesData.map((game) => game.get({ plain: true }));
+  //     res.render("dashboard", { games });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   });
 });
 
 module.exports = router;
